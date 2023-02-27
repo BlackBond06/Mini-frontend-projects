3d5cdecdb1ac42d64d1017fb569ed0a4bfb75bf6
BlackBond06 <eleazuosita007@yahoo.com>
2023-02-27 16:35:13 +0100

    updated commit

--- frontend/Javascript Projects/Pig--game/script.js
+++ frontend/Javascript Projects/Pig--game/script.js
@@ -7,18 +7,36 @@ const player1Score = document.getElementById("score--0");
 const player2Score = document.getElementById("score--1");
 const defaultScore = 0;
 const dice = document.querySelector(".dice");
+const player1CurrentScore = document.getElementById("current--0");
+
+let scores = [];
 
 
-/* Set game to starting point */
-newGame.addEventListener("click", ()=>{
+/* Set game initial state  on page load*/
+document.addEventListener("DOMContentLoaded", ()=>{
     player1Score.textContent = defaultScore;
     player2Score.textContent = defaultScore;  
     dice.style.display = "none";
+})
+/* Reset game to initial state */
+newGame.addEventListener("click", ()=>{
+    player1Score.textContent = defaultScore;
+    player2Score.textContent = defaultScore; 
+    player1CurrentScore.textContent = defaultScore;
+    dice.style.display = "none";
 });
 
+/* Begin game by rolling dice. player 1 starts the game */
 rollDice.addEventListener("click", ()=>{
-    const randomDice = Math.trunc(Math.random()* 5) + 1;
-    console.log(randomDice);
+    const randomDice = Math.trunc(Math.random()* 6) + 1;
     dice.style.display = "block";
-    dice.src = `dice-${randomDice}.png`
+    dice.src = `dice-${randomDice}.png`;
+    player1Score.textContent = randomDice;
+    player1CurrentScore.textContent = randomDice;
+    scores.push(randomDice);
+    const playerScore = scores.reduce((a, b)=> a + b, 0);
+    player1CurrentScore.textContent = playerScore;
+    // if(player1Score === 1){
+    //     player2Score.textContent = defaultScore;  
+    // }
 })
