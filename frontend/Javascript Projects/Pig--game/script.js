70f3b9d6e8c7d516f98c04150590b812cce73d41
BlackBond06 <eleazuosita007@yahoo.com>
2023-03-28 17:58:45 +0100

    completed

--- frontend/Javascript Projects/Pig--game/script.js
+++ frontend/Javascript Projects/Pig--game/script.js
@@ -16,11 +16,6 @@ const fileInputs = document.querySelectorAll(".input");
 const defaultScore = 0;
 /* Game set to starting conditions on page load */
 
-document.addEventListener("DOMContentLoaded", () => {
-  score0El.textContent = defaultScore;
-  score1El.textContent = defaultScore;
-  diceEl.classList.add("hidden");
-});
 
 /* Each player selects image*/
 const loadImage = (e) => {
@@ -31,22 +26,36 @@ const loadImage = (e) => {
   let img = document.getElementById("cta--img");
   let img1 = document.getElementById("cta--img1");
   id === "input1"
-    ? (img.src = URL.createObjectURL(file))
-    : (img1.src = URL.createObjectURL(file));
+  ? (img.src = URL.createObjectURL(file))
+  : (img1.src = URL.createObjectURL(file));
 };
 
 fileInputs.forEach((input) => {
   input.addEventListener("change", loadImage);
 });
 
-let currentScore = 0;
-let activePlayer = 0;
-let scores = [0, 0];
-let playing = true;
+let scores, currentScore, activePlayer, playing ;
+
+
+const init = () => {
+  scores = [defaultScore, defaultScore];
+  currentScore = defaultScore;
+  activePlayer = defaultScore;
+  playing = true;
+  score0El.textContent = defaultScore;
+  score1El.textContent = defaultScore;
+  current0El.textContent = defaultScore;
+  current1El.textContent = defaultScore;
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
 
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent =
-    defaultScore;
+  defaultScore;
   currentScore = defaultScore;
   activePlayer = activePlayer === 0 ? 1 : 0;
   player0El.classList.toggle("player--active");
@@ -60,7 +69,7 @@ btnRoll.addEventListener("click", () => {
     /* display dice */
     diceEl.classList.remove("hidden");
     diceEl.src = `dice-${randomDice}.png`;
-
+    
     /* check if player rolled 1 */
     if (randomDice !== 1) {
       /* add dice to current score */
@@ -79,33 +88,27 @@ btnHold.addEventListener("click", () => {
     // Add score to active player
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
-      scores[activePlayer];
-
+    scores[activePlayer];
+    
     // check if player score >= 100
-
+    
     if (scores[activePlayer] >= 20) {
       playing = false;
       diceEl.classList.add("hidden");
       document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add("player--winner");
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--winner");
       document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove("player--active");
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove("player--active");
     } else {
       switchPlayer();
     }
   }
 });
 
-btnNew.addEventListener("click", ()=>{
-  diceEl.classList.add("hidden");
-  score0El.textContent = defaultScore;
-  score1El.textContent = defaultScore;
-  scores = [0, 0];
-  console.log(scores[activePlayer], activePlayer);
-})
-
-
+btnNew.addEventListener("click", init);
 
 // console.log(activePlayer, currentScore);
+
+document.addEventListener("DOMContentLoaded", init());
